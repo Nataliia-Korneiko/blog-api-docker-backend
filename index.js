848e5eb0237839b7edd9261f89292c34e1952664
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2022-03-03 22:25:17 +0200

    add auth

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
   MONGO_PASSWORD,
 } = require('./config/config');
 const postRouter = require('./routes/postRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -30,6 +31,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/posts', postRouter);
+app.use('/api/v1/users', userRouter);
 
 const port = process.env.PORT || 3000;
 
